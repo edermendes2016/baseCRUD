76690989d73ebf12738d8e3707297cef97a9a750
edermendes2016 <ederkbb@gmail.com>
2017-02-23 13:58:26 -0400

    First

---
+++ appCRUD/src/appCRUD.Dominio/Modelos/Endereco.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appCRUD.Dominio.Modelos
+{
+    class Endereco
+    {
+        public Endereco()
+        {
+            Id = new Guid();
+        }
+
+        public Guid Id { get; set; }
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public int Numero { get; set; }
+        public string Bairro { get; set; }
+        public bool Ativo { get; set; }
+        public bool Excluido { get; set; }
+    }
+}
---
+++ appCRUD/src/appCRUD.Dominio/Modelos/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appCRUD.Dominio.Modelos
+{
+    class Usuario
+    {
+        public Usuario()
+        {
+            Id = new Guid();
+        }
+
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public bool Ativo { get; set; }
+        public bool Excuido { get; set; }
+    }
+}
